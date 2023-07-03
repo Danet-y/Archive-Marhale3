813ab4956e8ffd4e7ae61b84e90aa52eb6fb8555
Danet-y <83012523+Danet-y@users.noreply.github.com>
2023-07-03 21:36:29 +0330

    sub 2 (by manizare)

--- دوره27/روز دوم/سوال دوم/2.cpp
+++ دوره27/روز دوم/سوال دوم/2.cpp
@@ -1,54 +1,53 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define Tof_Io ios_base::sync_with_stdio(false);cin.tie(0) , cout.tie(0);
-#define pb push_back
-#define mk make_pair
-#define all(v) v.begin(),v.end()
-#define int long long int
-#define F first
-#define S second
-#define tagh2(j) j>>1
-#define zarb2(j) j<<1
-const int N=169,MX=2e4+23,mod=10289,mod1 = 1e9+7;
-int n, dp[N][N][N];
-int inv[N], fac[N];
-int fd=0;
-int pwe(int j,int p) {int ret=1;for(;p;p>>=1,j=1LL *j * j%mod1) if(p&1) ret=1LL  * ret  * j % mod1;return ret;}
-void modplus(int &j,int y){(j+=y)>=mod && (j-=mod);}
-bool isprime(int n){bool fl = 1;for (int i = 2; i <= sqrt(n)+1; i++){if(n%i==0){fl = 0;break;}}return fl;}
-void printt(bool fl){if(fl==1){cout << "YES";return;}cout << "NO";}
-int GCD(int a, int b) {return b?GCD(b,a%b):a;}
-int MOD(int a, int b=mod1){ int res = a + b; return (res >= mod? res - mod : res); }
-int LCM(int a, int b) {return a/GCD(a,b)*b;}
-int lis(vector<int>& v){if (v.size() == 0) {return 0;} vector<int> tail(v.size(), 0); int szgth = 1; tail[0] = v[0]; for (int i = 1; i < v.size(); i++) {auto b = tail.begin(), e = tail.begin() + szgth; auto it = lower_bound(b, e, v[i]); if (it == tail.begin() + szgth){tail[szgth++] = v[i];}else{*it = v[i];}} return szgth;}
-int yay(int n){return (1ull << n);}
-int C(int n, int k) { return (((inv[k]*inv[n-k])%mod1)*fac[n])%mod1;}
-void solve(int i,int j,int k){dp[i][j][k] =dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] + dp[i - 1][j - 1][k - 1];}
-void build(){fac[0] = 1;for (int i=1; i<N; i++) fac[i] = (fac[i-1]*i)%mod1;inv[N-1] = pwe(fac[N-1],mod1-2);for (int i=N-1; i>0; i--) {inv[i-1] = (inv[i]*i)%mod1;}}
-int cal(vector<int> arr1,vector<int> v,int sz){return memcmp(arr1.data(), v.data(), sz * sizeof(v[0])) == 0;}
-int ans;
-int32_t main()
-{
-	dp[0][0][0] = 1;
-	for (int i = 1; i < 145; i++) 
-	{
-		for (int j = 0; j <= i; j++) 
-		{
-			for (int k = 0; k <= i; k++) 
-			{
-				if (j > i-j or k > i-k) 
-				{
-					dp[i][j][k] = 0;
-				}
-				if(k > j) 
-				{
-					dp[i][j][k] = 0;
-					continue;
-				}
-				solve(i,j,k);
-				dp[i][j][k]=dp[i][j][k]%mod;
-			}
-		}
-	}
-	cout << dp[144][72][72];
+#include <bits/stdc++.h> 
+#define pb push_back 
+#define F first 
+#define S second 
+#define all(a) a.begin(),a.end() 
+#define pii pair<int,int>
+#define int long long
+#define ld long double
+#define mat vector<vector<int> > 
+using namespace std ;
+const int  maxn =102, maxq = 1e5 + 10,inf =1e9 + 10 , mod= 10289 ; 
+int po(int a ,int b){
+  if(b==0)return 1;
+  int v = po(a , b/2) ;
+  v = v * v % mod ;
+  if(b&1)v = (v*a)%mod ;
+  return v;
 }
+int f[maxn] , inv[maxn] ; 
+
+int c(int n , int k){
+  return (f[n] * inv[k] % mod * inv[n-k]%mod)%mod ;
+}
+int dp[maxn][maxn][maxn]  ;
+int kat(int n){
+  return (c(n , n/2) - c(n , n/2+1) + mod)%mod ;
+}
+signed main(){
+  ios::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+  int n = 70  ; 
+  f[0] = 1;
+  for(int i = 1; i <= n ; i++){
+    f[i] = f[i-1] * i % mod ;
+  }
+  for(int i = 0 ;i <= n ; i++){
+    inv[i] =po(f[i] , mod-2) ;
+  }
+  int ans =0  ;
+  for(int i = 0; i <= n-2 ; i++){
+    ans = (ans + kat(n-2) + mod) %mod ;
+    if(i%2==0){
+      ans =  (ans - kat(i) * kat(n-2-i)%mod + mod)%mod ;
+    }
+  }
+  cout << ans ; 
+}
+/*
+  
+  }
+*/
+
+
+
